236c133b65ab55f3ed3e62908caf0f8e773ccb19
srinath vunnava <84583149+srinathvunnava@users.noreply.github.com>
2022-02-03 13:11:40 -0600

    a vs link && link css and js file to html

---
+++ Js/onelinecode.js
@@ -0,0 +1,12 @@
+// <a> vs <link>
+// href is attribute 
+// https://stackoverflow.com/a/28650226/12573022
+// ================
+
+//Link style tag in Html 
+// <link rel="stylesheet" href="styles.css"/>
+
+// link js file in htl 
+// <script type="module" src='./index.js' ></script>
+
+//===================
